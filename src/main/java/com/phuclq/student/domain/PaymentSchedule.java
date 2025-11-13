22670e95030a021ba8a62a96e350241c89abac1f
phuc.lq <phuc.lq@aseansc.com.vn>
2025-11-13 17:00:20 +0700

    d

--- src/main/java/com/phuclq/student/domain/PaymentSchedule.java
+++ src/main/java/com/phuclq/student/domain/PaymentSchedule.java
@@ -46,13 +46,6 @@ public class PaymentSchedule extends Auditable<String> {
     @Column(name = "status")
     private String status;
 
-    /** Số tiền khách đã thanh toán (nếu có) */
-    @Column(name = "paid_amount")
-    private BigDecimal paidAmount;
-
-    /** Ngày thực tế khách thanh toán (nếu có) */
-    @Column(name = "paid_date")
-    private LocalDate paidDate;
 
     @OneToMany(fetch = FetchType.LAZY)
     @JoinColumn(name = "payment_schedule_id", insertable = false, updatable = false)
