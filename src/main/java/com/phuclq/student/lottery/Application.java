962b8d98a09f7982b6e9e2217e22a3f4ac637207
phuc.lq <phuc.lq@aseansc.com.vn>
2025-11-19 15:12:10 +0700

    d

--- src/main/java/com/phuclq/student/lottery/Application.java
+++
@@ -1,3 +0,0 @@
-package com.phuclq.student.lottery;
-import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication public class Application{public static void main(String[] a){SpringApplication.run(Application.class,a);}}
--- src/main/java/com/phuclq/student/lottery/lottery/Application.java
+++
@@ -1,1 +0,0 @@
-package com.phuclq.student.lottery.lottery;import org.springframework.boot.*;import org.springframework.boot.autoconfigure.*;@SpringBootApplication public class Application{public static void main(String[]a){SpringApplication.run(Application.class,a);}}
